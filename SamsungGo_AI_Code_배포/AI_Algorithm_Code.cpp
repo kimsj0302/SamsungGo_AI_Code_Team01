c3224398ffd225d6f3780bd348cf8b4dd7dd3020
99bottlesofbeer <41835126+99bottlesofbeer@users.noreply.github.com>
2018-08-01 20:08:06 +0900

    change

--- SamsungGo_AI_Code_배포/AI_Algorithm_Code.cpp
+++ SamsungGo_AI_Code_배포/AI_Algorithm_Code.cpp
@@ -8,180 +8,208 @@
 char info[] = { "TeamName:씨두박년아,Department:POSCAT" };
 
 // my code
-#include <utility>
 #include <queue>
-#include <vector>
-#include <functional>
 using namespace std;
 
-typedef pair<int, int> p;
+extern int op_x[2], op_y[2], op_cnt;
 
 int INF = 1e9;
 
-int dx[8] = { 1, 1, 0, -1, -1, -1,  0,  1};
-int dy[8] = { 0, 1, 1,  1,  0, -1, -1, -1};
+int dx[8] = { 1, 1, 0, -1, -1, -1,  0,  1 };
+int dy[8] = { 0, 1, 1,  1,  0, -1, -1, -1 };
 
-bool inb(int y, int x)
+bool inb(int x, int y)
 {
-    return 0 <= y && y < 19 && 0 <= x && x < 19;
+	return 0 <= x && x < 19 && 0 <= y && y < 19;
 }
 
 int s[6][6] =
 {
-    // 0,  1,  2,  3,   4,   5
-    {  0,  0,  0,  0,   0,   0 }, // 0
-    {  0,  0,  0,  0,   0,   0 }, // 1
-    {  1,  2,  3,  0,   0,   0 }, // 2
-    {  3,  4,  5,  6,   0,   0 }, // 3
-    {  6,  7,  8,  9, INF,   0 }, // 4
-    { 10, 11, 12, 13, INF, INF }  // 5
+	// 0,  1,  2,  3,   4,   5
+	{ 1,  1,  1,  4,   9,  16 }, // 0
+{ 1,  1,  2,  6,  12,  20 }, // 1
+{ 1,  2,  3,  8,  15,  24 }, // 2
+{ 4,  6,  8, 10,  18,  28 }, // 3
+{ 9, 12, 15, 18, INF, INF }, // 4
+{ 16, 20, 24, 28, INF, INF }  // 5
 };
 
-int score(int board[][19])
+extern int board[20][20];
+
+bool free(int x, int y)
 {
-    priority_queue<int> pq1;
-    for (int i = 0; i < 19; i++)
-    for (int j = 0; j < 19; j++)
-    if (board[i][j] == 1 || board[i][j] == 3)
-    for (int k = 0; k < 4; k++)
-    {
-        int len = 1;
-        while (1)
-        {
-            int y = i + len*dy[k], x = j + len*dx[k];
-            if (!inb(y, x)) break;
-            if (board[y][x] != 1 || board[y][x] != 3) break;
-            len++;
-        }
-        if (len >= 6) return INF;
-        pq1.push(len);
-        if (pq1.size() > 2) pq1.pop();
-    }
-    int a = pq1.top(); pq1.pop();
-    int b = pq1.top(); pq1.pop();
-
-    int ret = s[b][a];
-
-    priority_queue<int> pq2;
-    for (int i = 0; i < 19; i++)
-    for (int j = 0; j < 19; j++)
-    if (board[i][j] == 2 || board[i][j] == 3)
-    for (int k = 0; k < 4; k++)
-    {
-        int len = 1;
-        while (1)
-        {
-            int y = i + len*dy[k], x = j + len*dx[k];
-            if (!inb(y, x)) break;
-            if (board[y][x] != 2 || board[y][x] != 3) break;
-            len++;
-        }
-        if (len >= 6) return -INF;
-        pq2.push(len);
-        if (pq2.size() > 2) pq2.pop();
-    }
-    int c = pq2.top(); pq2.pop();
-    int d = pq2.top(); pq2.pop();
-
-    return ret - s[d][c];
+	return board[x][y] == 0;
 }
 
-bool surr(int board[][19], int y, int x)
+int score()
 {
-    for (int k = 0; k < 8; k++) if (inb(y + dy[k], x + dx[x]) && board[y + dy[k]][x + dx[k]]) return true;
-    return false;
+	int a = 0, b = 0;
+
+	for (int i = 0; i < 19; i++)
+		for (int j = 0; j < 19; j++)
+			if (board[i][j] == 1)
+				for (int k = 0; k < 4; k++)
+				{
+					int len = 1;
+					while (1)
+					{
+						int x = i + len * dx[k], y = j + len * dy[k];
+						if (!inb(x, y)) break;
+						if (board[x][y] != 1) break;
+						len++;
+					}
+					if (len >= 6) return INF;
+
+					if (len > a)
+					{
+						a = len;
+						b = a;
+					}
+					else if (len > b)
+					{
+						b = len;
+					}
+				}
+
+	int c = 0, d = 0;
+
+	for (int i = 0; i < 19; i++)
+		for (int j = 0; j < 19; j++)
+			if (board[i][j] == 2)
+				for (int k = 0; k < 4; k++)
+				{
+					int len = 1;
+					while (1)
+					{
+						int x = i + len * dx[k], y = j + len * dy[k];
+						if (!inb(x, y)) break;
+						if (board[x][y] != 2) break;
+						len++;
+					}
+					if (len >= 6) return -INF;
+
+					if (len > c)
+					{
+						c = len;
+						d = a;
+					}
+					else if (len > d)
+					{
+						d = len;
+					}
+				}
+
+
+	return s[b][a] - s[d][c];
 }
 
-int alphabeta(int board[][19], int depth, int alpha, int beta, bool playermax)
+bool surr(int x, int y)
 {
-    if (depth == 0) return score(board);
+	for (int k = 0; k < 8; k++)
+		if (inb(x + dx[k], y + dy[k]))
+			if (board[x + dx[k]][y + dy[k]] != 0) return true;
+	return false;
+}
 
-    if (playermax)
-    {
-        for (int y1 = 0; y1 < 18; y1++)
-        for (int x1 = 0; x1 < 19; x1++)
-        for (int y2 = y1 + 1; y2 < 19; y2++)
-        for (int x2 = 0; x2 < 19; x2++)
-        {
-            if (!isFree(y1, x1) || !surr(board, y1, x1) || !isFree(y2, x2) || !surr(board, y2, x2)) continue;
+int alphabeta(int px0, int py0, int px1, int py1, int depth, int alpha, int beta, bool playermax)
+{
+	int hscore = score();
+	if (depth == 0 || hscore > INF - 30 || hscore < -INF + 30) return hscore;
 
-            board[y1][x1] = board[y2][x2] = 1;
-            int v = alphabeta(board, depth - 1, alpha, beta, false);
+	if (playermax)
+	{
+		int v = -INF;
 
-            board[y1][x1] = board[y2][x2] = 0;
+		for (int x0 = px0 - 2; x0 <= px0 + 2; x0++)
+			for (int y0 = py0 - 2; y0 <= py0 + 2; y0++)
+				for (int x1 = px1 - 2; x1 <= px1 + 2; x1++)
+					for (int y1 = py1 - 2; y1 <= py1 + 2; y1++)
+					{
+						if (!inb(x0, y0) || !inb(x1, y1)) continue;
+						if (x0 == x1 && y0 == y1) continue;
+						if (!free(x0, y0) || !free(x1, y1)) continue;
 
-            if (alpha < v) alpha = v;
+						board[x0][y0] = board[x1][y1] = 1;
+						v = max(v, alphabeta(x0, y0, x1, y1, depth - 1, alpha, beta, false));
+						board[x0][y0] = board[x1][y1] = 0;
 
-            if (beta <= alpha) break;
-        }
+						alpha = max(alpha, v);
 
-        return alpha;
-    }
-    else
-    {
-        for (int y1 = 0; y1 < 18; y1++)
-        for (int x1 = 0; x1 < 19; x1++)
-        for (int y2 = y1 + 1; y2 < 19; y2++)
-        for (int x2 = 0; x2 < 19; x2++)
-        {
-            if (!isFree(y1, x1) || !surr(board, y1, x1) || !isFree(y2, x2) || !surr(board, y2, x2)) continue;
+						if (beta <= alpha) break;
+					}
 
-            board[y1][x1] = board[y2][x2] = 2;
-            int v = alphabeta(board, depth - 1, alpha, beta, true);
+		return v;
+	}
+	else
+	{
+		int v = INF;
 
-            board[y1][x1] = board[y2][x2] = 0;
+		for (int x0 = px0 - 2; x0 <= px0 + 2; x0++)
+			for (int y0 = py0 - 2; y0 <= py0 + 2; y0++)
+				for (int x1 = px1 - 2; x1 <= px1 + 2; x1++)
+					for (int y1 = py1 - 2; y1 <= py1 + 2; y1++)
+					{
+						if (x0 == x1 && y0 == y1) continue;
+						if (!isFree(x0, y0) || !isFree(x1, y1)) continue;
 
-            if (beta > v) beta = v;
+						board[x0][y0] = board[x1][y1] = 2;
+						v = min(v, alphabeta(x0, y0, x1, y1, depth - 1, alpha, beta, true));
+						board[x0][y0] = board[x1][y1] = 0;
 
-            if (beta <= alpha) break;
-        }
+						beta = min(beta, v);
 
-        return beta;
-    }
+						if (beta <= alpha) break;
+					}
+
+		return v;
+	}
 }
 
 void myturn(int cnt) {
 
-    int x[2], y[2];
+	int x[2], y[2];
 
-    // 이 부분에서 알고리즘 프로그램(AI)을 작성하십시오. 기본 제공된 코드를 수정 또는 삭제하고 본인이 코드를 사용하시면 됩니다.
+	// 이 부분에서 알고리즘 프로그램(AI)을 작성하십시오. 기본 제공된 코드를 수정 또는 삭제하고 본인이 코드를 사용하시면 됩니다.
 
-    if (cnt == 1)
-    {
-        x[0] = 9; y[0] = 9;
-        domymove(x, y, cnt);
-        return;
-    }
+	if (cnt == 1) // first start
+	{
+		x[0] = 9; y[0] = 9;
+		domymove(x, y, cnt);
+		return;
+	}
 
-    int myboard[19][19];
-    for (int i = 0; i < 19; i++)
-    for (int j = 0; j < 19; j++)
-    myboard[i][j] = showBoard(i, j);
+	int minscore = INF;
 
-    int maxscore = -INF;
+	for (int x0 = 0; x0 < 19; x0++)
+		for (int y0 = 0; y0 < 19; y0++)
+			for (int x1 = 0; x1 < 19; x1++)
+				for (int y1 = 0; y1 < 19; y1++)
+				{
+					if (x0 == x1 && y0 == y1) continue;
+					if (!free(x0, y0) || !free(x1, y1) || !surr(x0, y0) || !surr(x1, y1)) continue;
 
-    for (int y1 = 0; y1 < 18; y1++)
-    for (int x1 = 0; x1 < 19; x1++)
-    for (int y2 = y1 + 1; y2 < 19; y2++)
-    for (int x2 = 0; x2 < 19; x2++)
-    {
-        if (!isFree(y1, x1) || !surr(myboard, y1, x1) || !isFree(y2, x2) || !surr(myboard, y2, x2)) continue;
+					board[x0][y0] = board[x1][y1] = 1;
 
-        myboard[y1][x1] = myboard[y2][x2] = 1;
-        int v = alphabeta(myboard, 3, -INF, INF, true);
+					if (score() > INF - 30)
+					{
+						x[0] = x0; y[0] = y0; x[1] = x1; y[1] = y1;
+						domymove(x, y, cnt);
+						return;
+					}
 
-        myboard[y1][x1] = myboard[y2][x2] = 0;
+					int v = alphabeta(x0, y0, x1, y1, 2, -INF, INF, false);
 
-        if (maxscore < v)
-        {
-            maxscore = v;
-            x[0] = x1; y[0] = y1; x[1] = x2; y[1] = y2;
-        }
-    }
+					board[x0][y0] = board[x1][y1] = 0;
 
+					if (minscore > v)
+					{
+						minscore = v;
+						x[0] = x0; y[0] = y0; x[1] = x1; y[1] = y1;
+					}
+				}
 
-    // 이 부분에서 자신이 놓을 돌을 출력하십시오.
-    // 필수 함수 : domymove(x배열,y배열,배열크기)
-    // 여기서 배열크기(cnt)는 myturn()의 파라미터 cnt를 그대로 넣어야합니다.
-    domymove(x, y, cnt);
+	// 이 부분에서 자신이 놓을 돌을 출력하십시오.
+	// 필수 함수 : domymove(x배열,y배열,배열크기)
+	// 여기서 배열크기(cnt)는 myturn()의 파라미터 cnt를 그대로 넣어야합니다.
+	domymove(x, y, cnt);
 }
